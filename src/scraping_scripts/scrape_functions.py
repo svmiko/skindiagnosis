20c41f624775d76da7d96035099d795181b409f7
sumiko <tengwj01sumiko@gmail.com>
2023-10-25 00:25:25 +0800

    Updated Medline function

--- src/scraping_scripts/scrape_functions.py
+++ src/scraping_scripts/scrape_functions.py
@@ -191,19 +191,28 @@ def scrape_cdc(page):
     
 def scrape_medline(page):
     try: 
-        symptom_h = page.find(lambda tag: (tag.name in ['h2']) and 'symptoms' in tag.get_text().lower())
-        
-        if symptom_h:
-            symptoms_components = symptom_h.find_next('p')
-            #print(f"symptoms_components: {symptoms_components}")
-            if symptoms_components:
-                symptoms_text = symptoms_components.get_text()
-                #print(f"symptoms_text: {symptoms_text}")
-        else:
-            symptoms_components = page.find('p')
-            symptoms_text = symptoms_components.get_text()
-            symptoms_text = 'None'
-        return(symptoms_text)
+        symptom_h = page.find("div", id="section-2")
+        symptoms_components = symptom_h.find_all(lambda tag: (tag.name in ['p','ul']))
+        symptoms_text = ''
+        for sym_com in symptoms_components:
+            if sym_com.name == "ul":
+                lis = sym_com.find_all("li")
+                for li in lis:
+                    symptoms_text += li.get_text()
+            else:
+                symptoms_text += sym_com.get_text()
+        return symptoms_text
+        # if symptom_h:
+        #     symptoms_components = symptom_h.find_next('p')
+        #     #print(f"symptoms_components: {symptoms_components}")
+        #     if symptoms_components:
+        #         symptoms_text = symptoms_components.get_text()
+        #         #print(f"symptoms_text: {symptoms_text}")
+        # else:
+        #     symptoms_components = page.find('p')
+        #     symptoms_text = symptoms_components.get_text()
+        #     symptoms_text = 'None'
+        # return(symptoms_text)
     except:
         pass
 
@@ -355,16 +364,13 @@ def scrape_cedars_sinai(page):
 #skinsight
 def scrape_skinsight(page):
     try:
-        symptom_h = page.find(lambda tag: (tag.name in ['h1','h2','h3','section','div','p'])
-                                and 'symptoms' in tag.get_text().lower()
-                            )
-        #print(f"{keywords} {link_url} symptoms:")
-        symptoms_components = symptom_h.find_next(lambda tag: (tag.name in ['p','ul','h4']))
+        symptom_h = page.find('h2', string = "Signs & Symptoms")
+        symptoms_components = symptom_h.find_next_siblings(lambda tag: (tag.name in ['p','ul','h4']))
         if not symptoms_components:
             symptoms_components = symptom_h.find_next(lambda tag: (tag.name in ['div']))
             while not symptoms_components:
                 symptoms_components = symptoms_components.find_next(lambda tag: (tag.name in ['p','ul','div']))
-    
+
         symptoms_text = ''
         for sym_com in symptoms_components:
             symptoms_text += sym_com.get_text()
@@ -397,27 +403,20 @@ def scrape_dermnet(page):
 # import requests
 # from bs4 import BeautifulSoup
 
-# page_response = requests.get("https://www.healthline.com/health/dermatomyositis")
+# page_response = requests.get("https://medlineplus.gov/ency/article/000840.htm")
 
-# page_soup = BeautifulSoup(page_response.content, 'html.parser')
-#     # symptom_h = page.find(lambda tag: (tag.name in ['a'])
-#     #                                 and 'symptoms' in tag.get_text().lower()
-#     #                             )
-# symptom_h = page_soup.find('a', attrs={"name" : ["symptoms", "Symptoms"]})
+# page = BeautifulSoup(page_response.content, 'html.parser')
 
-# # print(f"{keyword} {link_url} symptoms:")
-# symptoms_components = symptom_h.find_parent('div')
-# symptoms_components = symptoms_components.find_parent('h2')
-# symptoms_components = symptoms_components.find_parent('div')
+# # symptom_h = page.find('h2', string = "Signs & Symptoms")
+# # symptoms_components = symptom_h.find_next_siblings(lambda tag: (tag.name in ['p','ul','h4']))
+# # if not symptoms_components:
+# #     symptoms_components = symptom_h.find_next(lambda tag: (tag.name in ['div']))
+# #     while not symptoms_components:
+# #         symptoms_components = symptoms_components.find_next(lambda tag: (tag.name in ['p','ul','div']))
 
-# symptoms_components = symptoms_components.find_next_siblings(lambda tag: (tag.name in ['p','ul','h4']))
-# if not symptoms_components:
-#     symptoms_components = symptom_h.find_next(lambda tag: (tag.name in ['p','ul']))
-#     while not symptoms_components:
-#         symptoms_components = symptoms_components.find_next(lambda tag: (tag.name in ['p','ul','div']))
-# symptoms_text = ''
-# for sym_com in symptoms_components:
-#     if "Continue reading about the symptoms" not in sym_com.get_text():
-#         symptoms_text += sym_com.get_text()
+# # symptoms_text = ''
+# # for sym_com in symptoms_components:
+# #     symptoms_text += sym_com.get_text()
 
-# print(symptoms_text)
+
+# print(scrape_medline(page))
